c60b492fe8993caaa193d3ed8728293bcba0d293
Junior <valdir.junior@mercadolivre.com>
2024-04-01 14:01:48 -0300

    feet: atividades do 1 ao 2 feitas.

---
+++ ex1.go
@@ -0,0 +1,12 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var (
+// 		nome  string = "Valdir Junior"
+// 		idade int    = 22
+// 	)
+
+// 	fmt.Printf("a %v tem %v anos. \n", nome, idade)
+// }
---
+++ main.go
@@ -0,0 +1,8 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var poteSorvete string
+// 	fmt.Println(poteSorvete)
+// }
